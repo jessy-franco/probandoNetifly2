af7d739328dfb28f9b0be172d220d8c3c0809699
JessyFranco <jesicafranco_1518@outlook.es>
2023-10-09 09:54:31 -0300

    cambie Buttons por buttos en ItemCount

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Button from "../Button/Buttons";
+import Button from "../Button/buttons";
 import "./itemcount.css";
 
 function ItemCount({ stock, onAddToCart, text }) {
